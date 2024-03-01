92095e5ed628b6729c21584d61d7a153b89b9d9a
Ben Hood <benhood97@btinternet.com>
2024-02-29 21:58:25 -0800

    add route for POST request and redirect user to index page

--- express_server.js
+++ express_server.js
@@ -50,6 +50,11 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${id}`);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  delete req.params.id && delete urlDatabase[req.params.id];
+  res.redirect(`/urls`);
+});
+
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);

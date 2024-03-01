4e4f0dcf3b947c6ff39386a46b96868ef19eeb1a
Ben Hood <benhood97@btinternet.com>
2024-02-29 19:12:21 -0800

    add app.post to log POST request to console. Implement function to create string of 6 random letters

--- express_server.js
+++ express_server.js
@@ -3,11 +3,17 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  const randomString = Math.random().toString(36).slice(2, 8);
+  return randomString;
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,12 +32,22 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' 
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
 
